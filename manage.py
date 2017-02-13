978541b7db8921c7327a9921d07ffd3e4eb3eccb
swnim <sngwon@madup.com>
2017-02-13 19:12:50 +0900

    FlaskDebugToolbar 추가

--- manage.py
+++ manage.py
@@ -1,8 +1,10 @@
+from flask_debugtoolbar import DebugToolbarExtension
 from flask_script import Manager
 from revenue import create_app
 
 app = create_app()
 manager = Manager(app)
+toolbar = DebugToolbarExtension(app)
 
 if __name__ == '__main__':
     manager.run()
